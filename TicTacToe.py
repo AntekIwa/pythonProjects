6288f9a4ab3785894dc64631f52e055b67dcc369
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2023-03-28 21:39:57 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,20 +17,25 @@ def ifWin():
             return 1
     return 2
 
+
 move = 0
 players = ['O', 'X'];
 
 while ifWin() == 1:
-    print(board[0], "-", board[1], "-", board[2])
-    print(board[3], "-", board[4], "-", board[5])
-    print(board[6], "-", board[7], "-", board[8])
+    print(board[0], " | ", board[1], " | ", board[2])
+    print("-------------")
+    print(board[3], " | ", board[4], " | ", board[5])
+    print("-------------")
+    print(board[6], " | ", board[7], " | ", board[8])
     move += 1
     position = int(input())
     board[position] = players[move % 2]
 
-print(board[0], "-", board[1], "-", board[2])
-print(board[3], "-", board[4], "-", board[5])
-print(board[6], "-", board[7], "-", board[8])
+print(board[0], " | ", board[1], " | ", board[2])
+print("-------------")
+print(board[3], " | ", board[4], " | ", board[5])
+print("-------------")
+print(board[6], " | ", board[7], " | ", board[8])
 
 if ifWin() == 2:
     print("draw")
