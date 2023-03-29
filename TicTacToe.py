f0578917b0dea25047215ada070301caf334726d
Filip Nowak <76557020+cyberfilip@users.noreply.github.com>
2023-03-29 17:04:26 +0200

    Update TicTacToe.py
    funkcja getMove oraz prtBoard

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,31 +4,16 @@ board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 # 3 4 5
 # 6 7 8
 
-def ifWin():
-    if (board[0] == board[1] and board[1] == board[2] and board[0] != ' ') or (board[3] == board[4] and board[4] == board[5] and board[3] != ' ') or (board[6] == board[7] and board[7] == board[8] and board[6] != ' '):
-        return 0
-    if (board[0] == board[3] and board[3] == board[6] and board[0] != ' ') or (board[1] == board[4] and board[4] == board[7] and board[1] != ' ') or (board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
-        return 0
-    if (board[0] == board[4] and board[0] == board[8] and board[0] != ' ') or (board[2] == board[4] and board[2] == board[6] and board[2] != ' '):
-        return 0
-    for i in range(0, 9):
-        if board[i] == ' ':
-            return 1
-    return 2
-
-
-move = 0
-players = ['O', 'X'];
-
-while ifWin() == 1:
+def prtBoard():
     print(board[0], " | ", board[1], " | ", board[2])
     print("-------------")
     print(board[3], " | ", board[4], " | ", board[5])
     print("-------------")
     print(board[6], " | ", board[7], " | ", board[8])
-    move += 1
-    ds = 0;
+
+def getMove():
     position = int(input())
+    ds = 0
     while (ds == 0):
         if (position < 0 or position > 8):
             print("wartość spoza zakresu")
@@ -38,13 +23,31 @@ while ifWin() == 1:
             position = int(input())
         else:
             ds = 1
-    board[position] = players[move % 2]
+    return position
 
-print(board[0], " | ", board[1], " | ", board[2])
-print("-------------")
-print(board[3], " | ", board[4], " | ", board[5])
-print("-------------")
-print(board[6], " | ", board[7], " | ", board[8])
+def ifWin():
+    if (board[0] == board[1] and board[1] == board[2] and board[0] != ' ') or (board[3] == board[4] and board[4] == board[5] and board[3] != ' ') or (board[6] == board[7] and board[7] == board[8] and board[6] != ' '):
+        return 0
+    if (board[0] == board[3] and board[3] == board[6] and board[0] != ' ') or (board[1] == board[4] and board[4] == board[7] and board[1] != ' ') or (board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
+        return 0
+    if (board[0] == board[4] and board[0] == board[8] and board[0] != ' ') or (board[2] == board[4] and board[2] == board[6] and board[2] != ' '):
+        return 0
+    for i in range(0, 9):
+        if board[i] == ' ':
+
+            return 1
+    return 2
+
+
+move = 0
+players = ['O', 'X'];
+
+while ifWin() == 1:
+    prtBoard()
+    move += 1
+    board[getMove()] = players[move % 2]
+
+prtBoard()
 
 if ifWin() == 2:
     print("draw")
